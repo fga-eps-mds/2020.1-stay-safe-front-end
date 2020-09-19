9cfdd316445045eb4cc125fb601f01fcaa738a3e
Daniel Primo <danielprimo9@gmail.com>
2020-09-19 13:10:45 -0300

    adding home.js and its route with maps and marker

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import { createStackNavigator } from '@react-navigation/stack'
 const AppStack = createStackNavigator()
 
 import Cadastro from './screens/Cadastro'
+import App from './screens/home'
 
 export default Routes = () => {
     return (
         <>
             <AppStack.Navigator screenOptions={{ headerShown: false }}>
                 <AppStack.Screen name="Cadastro" component={Cadastro} />
+                <AppStack.Screen name="Home" component={App} />
             </AppStack.Navigator>
         </>
     );
