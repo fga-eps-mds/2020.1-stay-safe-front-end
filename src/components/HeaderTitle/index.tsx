1d0745bc8c79f29e5afc7c524a951930824d8323
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-09-22 19:26:42 -0300

    Add the HeaderTitle component with his styles
    Co-authored-by: Daniel Primo <danielprimo9@gmail.com>

---
+++ src/components/HeaderTitle/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { HeaderTitleContainer, Title } from './styles';
+
+interface HeaderTitleProps {
+  text?: string;
+}
+
+const HeaderTitle: React.FC<HeaderTitleProps> = ({ text }) => {
+  return (
+    <HeaderTitleContainer>
+      <Title>{text}</Title>
+    </HeaderTitleContainer>
+  );
+}
+
+export default HeaderTitle;
