7741f8a1b2d35707e378a1e1a053233887e18b8b
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-09-22 19:27:31 -0300

    Add the Profile screen with styles
    Co-authored-by: Daniel Primo <danielprimo9@gmail.com>

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,21 +1,95 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as Font from 'expo-font';
-import { Container, Title } from './styles';
+import { Alert, SafeAreaView, Text } from 'react-native';
 
+import { 
+  Container, 
+  InputViewing,
+  LabelsContainer, 
+  LabelViewing, 
+  LogoWrapper,
+  EditButton
+} from './styles';
+import HeaderTitle from '../../components/HeaderTitle';
+import { KeyboardScrollView, SendLabel } from '../../components/NormalForms';
+
+import AsyncStorage from '@react-native-community/async-storage';
+import { getUser } from '../../services/users';
+
+import Logo from '../../img/logo.svg';
+
+import { scale } from '../../utils/scalling';
 
 const Profile: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [userFullName, setUserFullName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPwd, setUserPwd] = useState("");
+  const [userConfirmPwd, setUserConfirmPwd] = useState("");
+  
   const [loaded] = Font.useFonts({
-      'Trueno-SemiBold': require('../../fonts/TruenoSBd.otf'),
-      'Trueno-Regular': require('../../fonts/TruenoRg.otf'),
+    'Trueno-SemiBold': require('../../fonts/TruenoSBd.otf'),
+    'Trueno-Regular': require('../../fonts/TruenoRg.otf'),
   });
+ 
+  useEffect(() => {
+    AsyncStorage.getItem('username').then(username => {
+      getUser(username).then(response => {
+        if(response.status === 200) {
+          setUsername(response.body.username);
+          setUserFullName(response.body.full_name);
+          setUserEmail(response.body.email);
+        } else {
+          Alert.alert('Erro ao Visualizar usuário.');
+        }
+      });
+    });
+  }, []);
 
   if(!loaded)
     return null;
 
   return (
-    <Container style={{ justifyContent: "center", alignItems: "center" }}>
-      <Title> oi tudo bem? </Title>    
-    </Container> 
+    <SafeAreaView style={{ flex: 1 }}>
+      <KeyboardScrollView>
+        <Container>
+          <HeaderTitle text="Perfil" />
+          <LogoWrapper>
+            <Logo width={scale(100)} height={scale(100)} />
+          </LogoWrapper>
+          <LabelsContainer>
+            <LabelViewing>Username</LabelViewing>
+            <InputViewing
+              value={username}
+              onChange={text => setUsername(text)}
+            />
+            <LabelViewing>Nome Completo</LabelViewing>
+            <InputViewing 
+              value={userFullName} 
+              onChange={text => setUserFullName(text)} 
+            />
+            <LabelViewing>Email</LabelViewing>
+            <InputViewing
+              value={userEmail}
+              onChange={text => setUserEmail(text)}
+           />
+            <LabelViewing>Senha</LabelViewing>
+            <InputViewing 
+              value={userPwd}
+              onChange={text => setUserPwd(text)}
+            />
+            <LabelViewing>Confirmar senha</LabelViewing>
+            <InputViewing 
+              value={userConfirmPwd}
+              onChange={text => setUserConfirmPwd(text)}
+            />
+            <EditButton>
+              <SendLabel>Salvar</SendLabel>
+            </EditButton>
+          </LabelsContainer>
+        </Container>
+      </KeyboardScrollView>
+    </SafeAreaView>
   );
 }
 
--- src/screens/Profile/styles.ts
+++ src/screens/Profile/styles.ts
@@ -1,14 +1,40 @@
-import styled from 'styled-components';
-import { ViewProps, TextProps } from 'react-native';
+import styled from 'styled-components/native';
 
-export const Container = styled.view<ViewProps>`
+import { 
+  NormalLabel, 
+  NormalInput,
+  NormalSend
+} from '../../components/NormalForms';
+
+import { scale } from '../../utils/scalling';
+
+export const Container = styled.View`
   flex: 1;
+  width: 100%;
   background-color: #F0F0F5;
 `;
 
-export const Title = styled.text<TextProps>`
-  flex: 1;
-  background-color: #F0F0F5;
-  font-family: Trueno-Regular;
+export const LogoWrapper = styled.View`
+  margin-top: ${scale(35)}px;
+  justify-content: center;
+  align-items: center;
 `;
 
+export const LabelsContainer = styled.View`
+  height: 100%;
+  padding: ${scale(35)}px ${scale(25)}px;
+`;
+
+export const LabelViewing = styled(NormalLabel)`
+  margin-left: ${scale(10)}px;
+`;
+
+export const InputViewing = styled(NormalInput)`
+  width: 100%;
+  background-color: #FFFFFF;
+  height: ${scale(40)}px;
+`;
+
+export const EditButton = styled(NormalSend)`
+  width: 100%;
+`;
