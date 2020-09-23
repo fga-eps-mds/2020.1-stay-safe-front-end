f48d630c3ff9cb5e194b8427475e6c6e6d69218a
Herick Portugues <herick.portugues@gmail.com>
2020-09-23 17:57:32 -0300

    Create new file and add dropdownItems constants.
    Co-authored-by: Brenda Santos <brendasantosv.bs@gmail.com>

---
+++ src/screens/Occurrence/dropdownItems.ts
@@ -0,0 +1,70 @@
+export const occurrence_type = [
+    {
+        label: "Latrocínio",
+        value: "Latrocínio",
+    },
+    {
+        label: "Roubo a Traseunte",
+        value: "Roubo a Traseunte",
+    },
+    {
+        label: "Roubo de Veículo",
+        value: "Roubo de Veículo",
+    },
+    {
+        label: "Roubo de Residência",
+        value: "Roubo de Residência",
+    },
+    {
+        label: "Estupro",
+        value: "Estupro",
+    },
+]
+
+export const gun = [
+    {
+        label: "Nenhuma",
+        value: "None",
+    },
+    {
+        label: "Arma de Fogo",
+        value: "Fire",
+    },
+    {
+        label: "Arma Branca",
+        value: "White",
+    },
+]
+
+export const victim = [
+    {
+        label: "Vítima",
+        value: true,
+    },
+    {
+        label: "Testemunha",
+        value: false,
+    },
+]
+
+export const physicalAggression = [
+    {
+        label: "Sim",
+        value: true,
+    },
+    {
+        label: "Não",
+        value: false,
+    },
+]
+
+export const policeReport = [
+    {
+        label: "Sim",
+        value: true,
+    },
+    {
+        label: "Não",
+        value: false,
+    },
+]
