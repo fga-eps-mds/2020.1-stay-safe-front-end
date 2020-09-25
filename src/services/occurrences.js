c5761221e3ad5299063aaea0b25b3f0fc434b821
Herick Portugues <herick.portugues@gmail.com>
2020-09-24 21:58:15 -0300

    Add conection with user-service api and createOccurrence function.
    Co-authored-by: Brenda Santos <brendasantosv.bs@gmail.com>

---
+++ src/services/occurrences.js
@@ -0,0 +1,22 @@
+import { userApi } from './api'
+
+export const createOccurrence = async (data, token) => {
+    const response = await fetch(userApi + '/occurrences/', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `${token}`
+        },
+        body: JSON.stringify({
+            gun: data.gun,
+            location: data.location,
+            occurrence_date_time: data.occurrence_date_time,
+            occurrence_type: data.occurrence_type,
+            physical_aggression: data.physical_aggression,
+            police_report: data.police_report,
+            victim: data.victim
+        })
+    })
+    return { status: response.status, body: await response.json() }
+};
