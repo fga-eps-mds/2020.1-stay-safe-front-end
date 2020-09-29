0ae82030be3d66cfd9ea8b9b4602604fe81896f3
Daniel Primo <danielprimo9@gmail.com>
2020-09-29 18:35:42 -0300

    start heatmap test

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 import { StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import MapView from 'react-native-maps'
+import MapView, { Heatmap, PROVIDER_GOOGLE } from 'react-native-maps'
 
 import LoggedInModal from '../../components/LoggedInModal'
 
 export default Home = ({ navigation }) => {
+    points = [
+        { latitude: -15.9897883, longitude: -48.0464073, weight: 1 }
+    ];
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <MapView
@@ -21,6 +24,17 @@ export default Home = ({ navigation }) => {
                     coordinate={{
                         latitude: -15.9897883,
                         longitude: -48.0464073
+                    }}
+                />
+                <Heatmap
+                    points={this.points}
+                    radius={40}
+                    opacity={1}
+                    gradient={{
+                      colors: ["black", "purple", "red", "orange", "white"],
+                      startPoints: Platform.OS === 'ios' ? [0.01, 0.04, 0.1, 0.45, 0.5] :
+                        [0.1, 0.25, 0.5, 0.75, 1],
+                      colorMapSize: 2000
                     }}
                 />
             </MapView>
