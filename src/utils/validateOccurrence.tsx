08312d6a301bb9b945a3a89bbe61a9bec0902142
Rossicler Júnior <rossiclerjr@gmail.com>
2020-10-01 16:03:44 -0300

    Fixed eslint of utils folder

---
+++ src/utils/validateOccurrence.tsx
@@ -0,0 +1,23 @@
+import { Alert } from "react-native";
+
+export const validateOccurrence = (data) => {
+    const currentDate = new Date();
+    const error = ["Campo Inválido", ""];
+    if (data.occurrenceType === null || data.occurrenceType === "") {
+        error[1] = "Tipo de Ocorrência deve ser selecionado";
+    } else if (data.gun === null || data.gun === "") {
+        error[1] = "Tipo de Arma deve ser selecionado";
+    } else if (data.victim === null) {
+        error[1] = "Vitima deve ser selecionado";
+    } else if (data.physicalAggression === null) {
+        error[1] = "Agressão fisica deve ser selecionado";
+    } else if (data.policeReport === null) {
+        error[1] = "Boletim de ocorrência deve ser selecionado";
+    } else if (currentDate < data.occurrenceDateTime) {
+        error[1] = "O horário selecionado é maior que o atual";
+    }
+    if (error[1] !== "") {
+        Alert.alert(error[0], error[1]);
+    }
+    return error[1] === "";
+};
