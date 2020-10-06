d83898189df66f411f97fac7996c8a1df918df19
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-10-05 22:27:46 -0300

    Add the search options to filter crimes
    Co-authored-by: Daniel Primo <danielprimo9@gmail.com>

---
+++ src/screens/Home/searchOptions.ts
@@ -0,0 +1,27 @@
+export const searchOptions = [
+    {
+        id: 1,
+        name: "Latrocínio",
+        label: "Latrocinio",
+    },
+    {
+        id: 2,
+        name: "Roubo a Transeunte",
+        label: "Roubo a Transeunte",
+    },
+    {
+        id: 3,
+        name: "Roubo de Veículo",
+        label: "Roubo de Veiculo",
+    },
+    {
+        id: 4,
+        name: "Roubo de Residência",
+        label: "Roubo de Residencia",
+    },
+    {
+        id: 5,
+        name: "Estupro",
+        label: "Estupro",
+    },
+];
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components/native";
 import MapView from "react-native-maps";
+import Modal from "react-native-modalbox";
 
 import { scale } from "../../utils/scalling";
 
@@ -16,6 +17,32 @@ export const FilterButton = styled.TouchableOpacity`
     top: 8%;
     right: 7%;
     background-color: #ffffff;
-    z-index: 5;
+    z-index: 6;
     border-radius: ${scale(50)}px;
+`;
+
+export const FilterModal = styled(Modal)`
+    position: absolute;
+    height: ${scale(230)}px;
+    width: ${scale(300)}px;
+    margin-top: ${scale(50)}px;
+    border-radius: ${scale(30)}px;
+    padding: ${scale(20)}px ${scale(28)}px;
+`;
+
+export const ButtonOptionContainer = styled.View`
+    height: ${scale(24)}px;
+    margin-bottom: ${scale(16)}px;
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const OptionCircleButton = styled.TouchableOpacity`
+    justify-content: center;
+    align-items: center;
+    margin-right: ${scale(10)}px;
+`;
+
+export const ButtonOptionText = styled.Text`
+    font-size: ${scale(16)}px;
 `;
