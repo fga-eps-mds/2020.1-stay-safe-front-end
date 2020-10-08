5c70eb46f79c397ca67987fe0bff4bcf6380c22b
Herick Portugues <herick.portugues@gmail.com>
2020-10-07 22:15:32 -0300

    Add types to scalling variables.
    Co-authored-by: Italo Alves <italooko@gmail.com>

--- src/utils/scalling.tsx
+++ src/utils/scalling.tsx
@@ -4,12 +4,12 @@ const { width, height } = Dimensions.get("window");
 const guidelineBaseWidth = 350;
 const guidelineBaseHeight = 680;
 
-const scale = (size) => (width / guidelineBaseWidth) * size;
-const verticalScale = (size) => (height / guidelineBaseHeight) * size;
-const moderateScale = (size, factor = 0.5) =>
+const scale = (size: number) => (width / guidelineBaseWidth) * size;
+const verticalScale = (size: number) => (height / guidelineBaseHeight) * size;
+const moderateScale = (size: number, factor = 0.5) =>
     size + (scale(size) - size) * factor;
 
-const percentage = (percent) => (percent / 100) * width;
-const vPercentage = (percent) => (percent / 100) * height;
+const percentage = (percent: number) => (percent / 100) * width;
+const vPercentage = (percent: number) => (percent / 100) * height;
 
 export { scale, verticalScale, moderateScale, percentage, vPercentage };
