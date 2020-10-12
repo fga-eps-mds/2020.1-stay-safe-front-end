ec8369233d1c91ae7f0fc6108a83c6e2d8833f16
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-10-12 11:38:32 -0300

    Create the statistics card styles

---
+++ src/screens/CityStatistics/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+import { scale } from '../../utils/scalling';
+
+export const StatisticsCard = styled.View`
+    width: 80%;
+    background-color: #FFFFFF;
+    height: ${scale(415)}px;
+    border-radius: ${scale(20)}px;
+`;
