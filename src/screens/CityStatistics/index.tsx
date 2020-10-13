4686b716c1798d5f0e57fe2a4835a8b443eb535d
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-10-13 20:19:45 -0300

    Add the sort function and make the dropdown change the year

--- src/screens/CityStatistics/index.tsx
+++ src/screens/CityStatistics/index.tsx
@@ -1,9 +1,10 @@
 import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
 import { RouteProp, useRoute } from "@react-navigation/native";
-import React, { useCallback, useEffect, useState } from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import CircularLoader from "../../components/CircularLoader";
 import HeaderTitle from "../../components/HeaderTitle";
 import { Container, KeyboardScrollView } from "../../components/NormalForms";
 import { getAllOccurrences } from "../../services/occurrencesSecretary";
@@ -41,10 +42,12 @@ interface SecretaryOccurrence {
 }
 
 interface CityCrimes {
-    [city: string]: {
-        crime_nature: string;
-        quantity: number;
-    };
+    [city: string]: Array<Crime>;
+}
+
+interface Crime {
+    crime_nature: string;
+    quantity: number;
 }
 
 interface Year {
@@ -52,24 +55,81 @@ interface Year {
 }
 
 const CityStatistics: React.FC = () => {
-    const [selectedYear, setSelectedYear] = useState("");
+    const [selectedYear, setSelectedYear] = useState("2020");
     const route = useRoute<RouteProp<ParamList, "params">>();
 
     const cityName = route.params.city;
 
     const [data, setData] = useState<Data>([]);
 
-    const [cityStatistics, setCityStatistics] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [cityStatistics, setCityStatistics] = useState<Crime[]>([]);
+    const [higherStatistic, setHigherStatistic] = useState(0);
 
     useEffect(() => {
-        getAllOccurrences("df").then((response) => {
-            setData(response.body);
-        });
+        loadData();
     }, []);
 
     useEffect(() => {
-        console.log("trocou data2");
-    }, [data]);
+        loadData();
+    }, [selectedYear]);
+
+    const loadData = () => {
+        setIsLoading(true);
+        try {
+            getOccurrences().then((response) => {
+                if (response === 200) {
+                    setIsLoading(false);
+                }
+            });
+        } catch (error) {
+            Alert.alert("Erro ao conectar com o servidor.");
+        }
+    };
+
+    const getOccurrences = async () => {
+        const response = await getAllOccurrences("df");
+
+        if (response.status === 200) {
+            setData(response.body);
+
+            response.body.map((year: SecretaryOccurrence) => {
+                if (year.period.year === Number(selectedYear)) {
+                    year.cities.map((city) => {
+                        if (city[cityName]) {
+                            setCityStatistics(
+                                city[cityName].sort(function (
+                                    a: Crime,
+                                    b: Crime
+                                ) {
+                                    return b.quantity - a.quantity;
+                                })
+                            );
+
+                            let higher = -1;
+                            let higherName = "";
+
+                            city[cityName].map((crime: Crime) => {
+                                if (higher < crime.quantity) {
+                                    higher = crime.quantity;
+                                    higherName = crime.crime_nature;
+                                }
+                            });
+
+                            setHigherStatistic(
+                                city[cityName].filter(
+                                    (city: Crime) =>
+                                        city.crime_nature === higherName
+                                )[0].quantity
+                            );
+                        }
+                    });
+                }
+            });
+        }
+        return response.status;
+    };
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
@@ -94,42 +154,44 @@ const CityStatistics: React.FC = () => {
                                 }
                             />
                         </YearContainer>
-                        <CrimeStatistics>
-                            <CrimeContainer>
-                                <CrimeText>Latrocínio</CrimeText>
-                                <View style={{ flexDirection: "row" }}>
-                                    <CrimeBar percentage={100} />
-                                    <CrimeBarNumber>37</CrimeBarNumber>
-                                </View>
-                            </CrimeContainer>
-                            <CrimeContainer>
-                                <CrimeText>Roubo a Transeunte</CrimeText>
-                                <View style={{ flexDirection: "row" }}>
-                                    <CrimeBar percentage={95} />
-                                    <CrimeBarNumber>20</CrimeBarNumber>
-                                </View>
-                            </CrimeContainer>
-                            <CrimeContainer>
-                                <CrimeText>Roubo de Veículo</CrimeText>
-                                <View style={{ flexDirection: "row" }}>
-                                    <CrimeBar percentage={40} />
-                                    <CrimeBarNumber>15</CrimeBarNumber>
-                                </View>
-                            </CrimeContainer>
-                            <CrimeContainer>
-                                <CrimeText>Roubo de Residência</CrimeText>
-                                <View style={{ flexDirection: "row" }}>
-                                    <CrimeBar percentage={70} />
-                                    <CrimeBarNumber>26</CrimeBarNumber>
-                                </View>
-                            </CrimeContainer>
-                            <CrimeContainer>
-                                <CrimeText>Estupro</CrimeText>
-                                <View style={{ flexDirection: "row" }}>
-                                    <CrimeBar percentage={13.513513513} />
-                                    <CrimeBarNumber>5</CrimeBarNumber>
-                                </View>
-                            </CrimeContainer>
+                        <CrimeStatistics loading={isLoading}>
+                            {isLoading ? (
+                                <CircularLoader />
+                            ) : (
+                                cityStatistics.map((cityStatistic) => {
+                                    const percentage =
+                                        (cityStatistic.quantity /
+                                            higherStatistic) *
+                                        100.0;
+
+                                    if (
+                                        cityStatistic.crime_nature !==
+                                        "Trafico de Entorpecentes"
+                                    ) {
+                                        return (
+                                            <CrimeContainer
+                                                key={cityStatistic.crime_nature}
+                                            >
+                                                <CrimeText>
+                                                    {cityStatistic.crime_nature}
+                                                </CrimeText>
+                                                <View
+                                                    style={{
+                                                        flexDirection: "row",
+                                                    }}
+                                                >
+                                                    <CrimeBar
+                                                        percentage={percentage}
+                                                    />
+                                                    <CrimeBarNumber>
+                                                        {cityStatistic.quantity}
+                                                    </CrimeBarNumber>
+                                                </View>
+                                            </CrimeContainer>
+                                        );
+                                    }
+                                })
+                            )}
                         </CrimeStatistics>
                     </StatisticsCard>
                     <SortButton>
