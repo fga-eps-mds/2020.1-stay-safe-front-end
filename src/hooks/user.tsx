c9c7d9e95225a77bcdcde171832be92b3aec7ed5
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-10-15 15:35:43 -0300

    Add the signIn and signOut functions to user hook

--- src/hooks/user.tsx
+++ src/hooks/user.tsx
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-community/async-storage";
 import React, {
     createContext,
     useCallback,
@@ -5,14 +6,25 @@ import React, {
     useEffect,
     useContext,
 } from "react";
+import { Alert } from "react-native";
 import { ThemeProvider } from "styled-components";
 
+import { authUser } from "../services/users";
 import staySafeDarkTheme from "../styles/staySafeDarkTheme";
 import staySafeTheme from "../styles/staySafeTheme";
+
+interface SignInCredentials {
+    username: string;
+    password: string;
+}
 
 interface UserContextData {
     switchTheme: () => void;
     theme: AppTheme;
+    data: UserState;
+    isLoading: boolean;
+    signIn(credentials: SignInCredentials): Promise<void>;
+    signOut(): void;
 }
 
 interface AppTheme {
@@ -30,11 +42,69 @@ interface AppTheme {
     heatMapRed: string;
 }
 
+interface UserState {
+    username: string;
+    token: string;
+}
+
 const UserContext = createContext<UserContextData | null>(null);
 
 export const UserProvider: React.FC = ({ children }) => {
     const [theme, setTheme] = useState(staySafeTheme);
-    console.log("Tema do hook => ", theme);
+
+    const [data, setData] = useState<UserState>({ token: "", username: "" });
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadStorageData(): Promise<void> {
+            const [token, username] = await AsyncStorage.multiGet([
+                "@StaySafe:token",
+                "StaySafe:username",
+            ]);
+
+            console.log(token, username);
+
+            if (token[1] && username[1]) {
+                setData({ token: token[1], username: username[1] });
+            }
+
+            setIsLoading(false);
+        }
+
+        setTimeout(() => {
+            loadStorageData();
+        }, 1000);
+    }, []);
+
+    const signIn = useCallback(async ({ username, password }) => {
+        const response = await authUser({
+            username,
+            password,
+        });
+
+        if (!response.body.error && response.status === 200) {
+            const { token } = response.body;
+
+            await AsyncStorage.multiSet([
+                ["@StaySafe:token", token],
+                ["StaySafe:username", username],
+            ]);
+
+            setData({ username, token });
+        } else {
+            Alert.alert("Erro ao logar usuário", response.body.error);
+        }
+    }, []);
+
+    const signOut = useCallback(async () => {
+        await AsyncStorage.multiRemove([
+            "@StaySafe:token",
+            "StaySafe:username",
+        ]);
+
+        setData({ token: "", username: "" });
+    }, []);
 
     const switchTheme = useCallback(
         () =>
@@ -45,7 +115,9 @@ export const UserProvider: React.FC = ({ children }) => {
     );
 
     return (
-        <UserContext.Provider value={{ switchTheme, theme }}>
+        <UserContext.Provider
+            value={{ data, switchTheme, theme, isLoading, signIn, signOut }}
+        >
             <ThemeProvider theme={theme}>{children}</ThemeProvider>
         </UserContext.Provider>
     );
