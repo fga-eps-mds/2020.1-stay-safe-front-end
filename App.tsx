cfa46e6a46f0f1fc2f4d37104d20a76101a4a97e
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-10-14 22:36:47 -0300

    Provider theme to all screens

--- App.tsx
+++ App.tsx
@@ -1,13 +1,41 @@
-import 'react-native-gesture-handler'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer } from "@react-navigation/native";
+import React, { useCallback, useState, createContext, useContext } from "react";
+import { ThemeProvider } from "styled-components";
 
-import Routes from './src/routes'
+import Routes from "./src/routes";
+import staySafeDarkTheme from "./src/styles/staySafeDarkTheme";
+import staySafeTheme from "./src/styles/staySafeTheme";
 
-export default function App() {
+const GeneralAppContext = createContext();
+
+export function useGeneralAppContext() {
+    return useContext(GeneralAppContext);
+}
+
+const App = () => {
+    const [theme, setTheme] = useState(staySafeTheme);
+
+    const switchTheme = useCallback(
+        () =>
+            setTheme(
+                theme === staySafeTheme ? staySafeDarkTheme : staySafeTheme
+            ),
+        [theme]
+    );
+
     return (
         <NavigationContainer>
-            <Routes />
+            <ThemeProvider theme={theme}>
+                <GeneralAppContext.Provider
+                    value={{
+                        switchTheme,
+                    }}
+                >
+                    <Routes />
+                </GeneralAppContext.Provider>
+            </ThemeProvider>
         </NavigationContainer>
     );
-}
+};
+
+export default App;
