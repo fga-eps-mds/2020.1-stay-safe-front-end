45329a7b632d47df5981c4e792b20b6b46395b02
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-10-14 22:35:27 -0300

    Create a context for theme and dark theme selection

---
+++ src/styles/staySafeDarkTheme.ts
@@ -0,0 +1,15 @@
+export default {
+    type: "dark",
+    primaryDarkBlue: "#8257E6",
+    primarySuperDarkBlue: "#BE375F",
+    primaryLightBlue: "#ED8554",
+    primaryWhite: "#000000",
+    primaryRed: "#E4443F",
+    primaryGray: "#F5EB6D",
+    primaryBackground: "#8C8C8C",
+    primaryBlack: "#FFFFFF",
+
+    heatMapGreen: "rgba(101, 239, 0, 0.6)",
+    heatMapOrange: "rgba(232, 140, 0, 0.6)",
+    heatMapRed: "rgba(239, 0, 0, 0.6)",
+};
---
+++ src/styles/staySafeTheme.ts
@@ -0,0 +1,15 @@
+export default {
+    type: "default",
+    primaryDarkBlue: "#011640",
+    primarySuperDarkBlue: "#010A26",
+    primaryLightBlue: "#7DBFE2",
+    primaryWhite: "#FFFFFF",
+    primaryRed: "#E83338",
+    primaryGray: "#C8C8C8",
+    primaryBackground: "#F0F0F5",
+    primaryBlack: "#000000",
+
+    heatMapGreen: "rgba(101, 239, 0, 0.6)",
+    heatMapOrange: "rgba(232, 140, 0, 0.6)",
+    heatMapRed: "rgba(239, 0, 0, 0.6)",
+};
