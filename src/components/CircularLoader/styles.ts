c79d1d97eb959152944a33a4f32abe4f0a979ff9
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-10-18 13:35:32 -0300

    Add a normal padding do circular loader component

--- src/components/CircularLoader/styles.ts
+++ src/components/CircularLoader/styles.ts
@@ -1,7 +1,10 @@
 import styled from "styled-components/native";
 
+import { scale } from "../../utils/scalling";
+
 export const Container = styled.View`
     flex: 1;
     align-items: center;
     justify-content: center;
+    padding: ${scale(9)}px;
 `;
