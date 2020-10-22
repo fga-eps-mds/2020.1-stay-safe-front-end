602c327d7b603342e5f10f27d37e5564b25403ed
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-10-22 12:49:48 -0300

    Adapt the secretary requests to the new format

--- src/services/occurrencesSecretary.tsx
+++ src/services/occurrencesSecretary.tsx
@@ -2,28 +2,65 @@ import { secretaryApi } from "./api";
 
 export const getOccurrencesByCrimeNature = async (
     uf: string,
-    crime: string
+    crime: string,
+    initialMonth: string,
+    finalMonth: string,
+    perCapita: number
 ) => {
     const response = await fetch(
-        secretaryApi + `?secretary=${uf}&crime=${crime}`,
+        secretaryApi +
+            `?secretary=${uf}&initial_month=${initialMonth}&final_month=${finalMonth}&crime=${crime}`,
         {
             method: "GET",
             headers: {
                 Accept: "application/json",
                 "Content-Type": "application/json",
+                per_capita: perCapita,
             },
         }
     );
     return { status: response.status, body: await response.json() };
 };
 
-export const getAllOccurrences = async (uf: string) => {
-    const response = await fetch(secretaryApi + `?secretary=${uf}`, {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-    });
+export const getAllOccurrencesOfCity = async (
+    uf: string,
+    city: string,
+    initialMonth: string,
+    finalMonth: string,
+    perCapita: number
+) => {
+    const response = await fetch(
+        secretaryApi +
+            `?secretary=${uf}&city=${city}&initial_month=${initialMonth}&final_month=${finalMonth}`,
+        {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                per_capita: perCapita,
+            },
+        }
+    );
+    return { status: response.status, body: await response.json() };
+};
+
+export const getAllOccurrence = async (
+    uf: string,
+    initialMonth: string,
+    finalMonth: string,
+    perCapita: number
+) => {
+    const response = await fetch(
+        secretaryApi +
+            `?secretary=${uf}&initial_month=${initialMonth}&final_month=${finalMonth}`,
+        {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                per_capita: perCapita,
+            },
+        }
+    );
     return { status: response.status, body: await response.json() };
 };
