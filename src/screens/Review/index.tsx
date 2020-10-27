fd58da7e31cc643a9023b9be1cb7bb666c2f6107
danieldagerom <danielprimo9@gmail.com>
2020-10-27 17:36:42 -0300

    add starting files

---
+++ src/screens/Review/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Text, View } from "react-native"
+
+const Review: React.FC = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <Text>
+        Review ----------
+      </Text>
+    </View>
+  )
+}
+
+export default Review;
+
