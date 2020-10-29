f78faca1462eaf80d924895d193d1fa47c58ce4f
danieldagerom <danielprimo9@gmail.com>
2020-10-29 18:39:30 -0300

    add new styles to review screen
    Co-authored-by: Brenda Santos <brendasantosv.bs@gmail.com>

---
+++ src/screens/Review/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components/native";
+import { scale } from "../../utils/scalling";
+
+export const NeighborhoodTitle = styled.Text`
+  font-size: ${scale(16)}px;
+  font-family: "Trueno-Regular";
+  color: ${(props) => props.theme.primarySuperDarkBlue};
+  text-align: center;
+`;
+
+export const NeighborhoodCard = styled.TouchableOpacity`
+    background-color: ${(props) => props.theme.primaryWhite};
+    width: 85%;
+    padding: ${scale(14)}px;
+    margin-vertical: ${scale(5)}px;
+    border-radius: ${scale(20)}px; 
+    align-items: center;
+`;
