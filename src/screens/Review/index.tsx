fce1a7a55b8feb9fe30388c422ee50d590859933
danieldagerom <danielprimo9@gmail.com>
2020-10-29 18:41:13 -0300

    create review index screen
    Co-authored-by: Brenda Santos <brendasantosv.bs@gmail.com>

--- src/screens/Review/index.tsx
+++ src/screens/Review/index.tsx
@@ -1,14 +1,75 @@
-import React from "react"
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
+import React, { useEffect, useState } from "react"
 import { Text, View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context";
+import HeaderTitle from "../../components/HeaderTitle";
+import { Container, KeyboardScrollView } from "../../components/NormalForms";
+
+import { getCityNeighborhoods } from "../../services/neighborhood";
+import { NeighborhoodCard, NeighborhoodTitle } from "./styles"
+
+interface Neighborhood {
+    city: string;
+    state: string;
+    average: number;
+    lighting: number;
+    movement: number;
+    neighborhood: string;
+    police: number;
+}
+type ParamList = {
+    params: {
+        cityName: string;
+        uf: string;
+    };
+};
+
 
 const Review: React.FC = () => {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
-      <Text>
-        Review ----------
-      </Text>
-    </View>
-  )
+    const navigation = useNavigation();
+
+    const route = useRoute<RouteProp<ParamList, "params">>(); 
+
+    const city = route.params.cityName;
+    const uf = route.params.uf.toUpperCase();
+
+    const [neighborhoods, setNeighborhoods] = useState<Neighborhood[]>([]);
+
+    useEffect(() => {
+        getNeighborhood()
+    }, []); 
+
+    const getNeighborhood = async () => {
+        const response = await getCityNeighborhoods(city, uf);
+
+        if(response.status === 200) {
+            setNeighborhoods(response.body);
+            console.log(response.body)
+        }
+    };
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+           <Container>
+                <HeaderTitle text={`Bairros - ${city}`} goBack/>
+                <KeyboardScrollView>
+                    {neighborhoods.map((neighborhood) => {
+                        return(
+                            <NeighborhoodCard style={{ elevation: 5 }} 
+                                onPress={() => navigation.navigate("NeighborhoodReview", {neighborhood})} key={neighborhood.neighborhood}>
+                                <NeighborhoodTitle>
+                                    {neighborhood.neighborhood}
+                                </NeighborhoodTitle>
+                            </NeighborhoodCard>
+                        )
+                    })} 
+                    
+                </KeyboardScrollView>
+           </Container>
+               
+
+        </SafeAreaView>
+    );
 }
 
 export default Review;
