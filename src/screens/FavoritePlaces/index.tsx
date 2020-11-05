844f68df45e5d8779686b914fb16e9c8a444bfe9
Herick Portugues <herick.portugues@gmail.com>
2020-11-05 16:22:40 -0300

    Create functionality to delete favorite places.
    Co-authored-by: Daniel Primo <danielprimo9@gmail.com>

--- src/screens/FavoritePlaces/index.tsx
+++ src/screens/FavoritePlaces/index.tsx
@@ -5,11 +5,16 @@ import { useTheme } from "styled-components";
 
 import HeaderTitle from "../../components/HeaderTitle";
 import { Container, KeyboardScrollView } from "../../components/NormalForms";
+import StayAlert from "../../components/StayAlert";
 import { useUser } from "../../hooks/user";
-import { getFavoritePlaces } from "../../services/favoritePlaces";
+import {
+    getFavoritePlaces,
+    deleteFavoritePlace,
+} from "../../services/favoritePlaces";
 import { PlaceCard, PlaceTitle, DeletePlace } from "./styles";
 
 interface FavoritePlace {
+    id_place: number;
     name: string;
     latitude: number;
     longitude: number;
@@ -20,6 +25,9 @@ const FavoritePlaces: React.FC = () => {
     const { data } = useUser();
 
     const [favoritePlaces, setFavoritePlaces] = useState<FavoritePlace[]>([]);
+
+    const [showDeleteModal, setDeleteModal] = useState(false);
+    const [idPlace, setIdPlace] = useState(0);
 
     useEffect(() => {
         getPlaces();
@@ -35,6 +43,21 @@ const FavoritePlaces: React.FC = () => {
         }
     };
 
+    const handleDelete = async (idPlace: number) => {
+        if (data.token !== "") {
+            const response = await deleteFavoritePlace(idPlace, data.token);
+
+            if (response.status === 204) {
+                setFavoritePlaces(
+                    favoritePlaces.filter(
+                        (place: FavoritePlace) => place.id_place !== idPlace
+                    )
+                );
+                setDeleteModal(false);
+            }
+        }
+    };
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <Container>
@@ -44,11 +67,16 @@ const FavoritePlaces: React.FC = () => {
                         return (
                             <PlaceCard
                                 style={{ elevation: 5 }}
-                                key={place.name}
+                                key={place.id_place}
                             >
                                 <PlaceTitle>{place.name}</PlaceTitle>
 
-                                <DeletePlace onPress={() => {}}>
+                                <DeletePlace
+                                    onPress={() => {
+                                        setDeleteModal(true);
+                                        setIdPlace(place.id_place);
+                                    }}
+                                >
                                     <Feather
                                         name="trash-2"
                                         size={22}
@@ -58,6 +86,18 @@ const FavoritePlaces: React.FC = () => {
                             </PlaceCard>
                         );
                     })}
+                    <StayAlert
+                        show={showDeleteModal}
+                        title="Apagar Local Favorito"
+                        message="Tem certeza que deseja apagar esse local? A ação não pode ser desfeita."
+                        showConfirmButton
+                        confirmText="Apagar"
+                        onConfirmPressed={() => handleDelete(idPlace)}
+                        showCancelButton
+                        cancelText="Cancelar"
+                        onCancelPressed={() => setDeleteModal(false)}
+                        onDismiss={() => setDeleteModal(false)}
+                    />
                 </KeyboardScrollView>
             </Container>
         </SafeAreaView>
--- src/services/favoritePlaces.ts
+++ src/services/favoritePlaces.ts
@@ -10,3 +10,14 @@ export const getFavoritePlaces = async (token: string) => {
     });
     return { status: response.status, body: await response.json() };
 };
+
+export const deleteFavoritePlace = async (idPlace: number, token: string) => {
+    const response = await fetch(userApi + `/places/${idPlace}`, {
+        method: "DELETE",
+        headers: {
+            Accept: "application/json",
+            Authorization: `${token}`,
+        },
+    });
+    return { status: response.status };
+};
