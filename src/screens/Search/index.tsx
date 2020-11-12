7ba867da05e372451c1904808fda26a097ff1606
danieldagerom <danielprimo9@gmail.com>
2020-11-12 15:56:42 -0300

    start search screen index

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useTheme } from "styled-components";
+
+import HeaderTitle from "../../components/HeaderTitle";
+
+import { Searchbar } from 'react-native-paper';
+import { scale } from "../../utils/scalling";
+import { SearchTabContainer } from "./styles"
 
 const Search: React.FC = () => {
-    return <></>;
+    const [searchQuery, setSearchQuery] = React.useState('');
+
+    const onChangeSearch = query => setSearchQuery(query);
+    
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <HeaderTitle text="Pesquise Aqui" goBack />
+                <SearchTabContainer>
+                    <Searchbar
+                        placeholder="Pesquise"
+                        onChangeText={onChangeSearch}
+                        value={searchQuery}
+                    />
+                </SearchTabContainer>
+        </SafeAreaView>
+    );
 };
 
 export default Search;
