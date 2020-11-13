d4f55fb34d99a5f564aae345c7615b5923de10aa
danieldagerom <danielprimo9@gmail.com>
2020-11-13 16:03:01 -0300

    add new logic of search
    Co-authored-by: Lucas Boaventura <lucasxboaventura@hotmail.com>

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,31 +1,99 @@
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useTheme } from "styled-components";
-
 import HeaderTitle from "../../components/HeaderTitle";
-
 import { Searchbar } from 'react-native-paper';
 import { scale } from "../../utils/scalling";
 import { SearchTabContainer } from "./styles"
+import { KeyboardScrollView } from "../../components/NormalForms";
+import { UfDropDown, DropDownsContainer, CityDropDown } from "./styles";
+
+import { ufs } from './searchConstants';
+import { useTheme } from "styled-components";
+
+interface IBGEUFResponse {
+    sigla: string;
+    nome: string;
+}
+
+interface IBGECityResponse {
+    nome: string;
+
+}
+
+interface DropDownItem {
+    label: string;
+    value: string;
+}
 
 const Search: React.FC = () => {
-    const [searchQuery, setSearchQuery] = React.useState('');
+    const theme = useTheme();
+    const [searchQuery, setSearchQuery] = useState('');
 
-    const onChangeSearch = query => setSearchQuery(query);
-    
+    const onChangeSearch = query => console.log(query);
+
+    const [SpCities, setSpCities] = useState<DropDownItem[]>([]);
+    const [Dfcities, DfsetCities] = useState<DropDownItem[]>([]);
+
+    const [selectedUf, setSelectedUf] = useState('');
+    const [selectedCity, setSelectedCity] = useState('');
+
+    useEffect(() => {
+        fetch(
+            'https://servicodados.ibge.gov.br/api/v1/localidades/estados/SP/municipios'
+        )
+            .then(async (response) => {
+                const responseOfIBGE = await response.json();
+                const citiesInitials = responseOfIBGE.map((city: IBGECityResponse) => {
+                   return { label: city.nome, value: city.nome };
+                });
+                setSpCities(citiesInitials);
+            });
+    }, []);
+
+    useEffect(() => { console.log({ selectedUf, selectedCity }) }, [selectedCity]);
+
     return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <HeaderTitle text="Pesquise Aqui" goBack />
-                <SearchTabContainer>
+        <SafeAreaView style={{ flex: 1, backgroundColor: theme.primaryBackground }}>
+            <HeaderTitle text="Pesquisar" />
+            <KeyboardScrollView>
+                {/* <SearchTabContainer>
                     <Searchbar
-                        placeholder="Pesquise"
+                        placeholder="Cidades..."
                         onChangeText={onChangeSearch}
                         value={searchQuery}
+                        onIconPress={() => console.log('oi')}
+                        inputStyle={{}}
                     />
-                </SearchTabContainer>
+                </SearchTabContainer> */}
+                <DropDownsContainer>
+                    {/* <View style={{ width: '30%', justifyContent: 'center', alignItems: 'center' }}>  */}
+                        <UfDropDown
+                            items={ufs}
+                            onChangeItem={(item: DropDownItem) => {
+                                setSelectedUf(item.label);
+                            }}
+                            dropdownStyle={{ height: scale(200) }}
+                            /* style={{ width: '50%' }} */
+                        />
+                    {/* </View> */}
+                    {/* <View style={{ width: '70%', justifyContent: 'center', alignItems: 'center' }} > */}
+                        <CityDropDown
+                            items={SpCities}
+                            onChangeItem={(item: DropDownItem) =>
+                                setSelectedCity(item.label)
+                            }
+                            style={{ width: '100%' }}
+                            disabled={selectedUf === ''}
+                            searchable={true}
+                            searchablePlaceholder="Digite uma cidade..."
+                            searchablePlaceholderTextColor="gray"
+                            /* seachableStyle={{}} */
+                            searchableError={() => <Text>Não encontrado</Text>}
+                        />
+                    {/* </View> */}
+                </DropDownsContainer>
+            </KeyboardScrollView>
         </SafeAreaView>
     );
 };
