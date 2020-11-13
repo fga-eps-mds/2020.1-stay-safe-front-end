96341ec5af27128e70efb62548dbec6b72c8310d
danieldagerom <danielprimo9@gmail.com>
2020-11-13 16:21:07 -0300

    add styles to the new search screen
    Co-authored-by: Lucas Boaventura <lucasxboaventura@hotmail.com>

--- src/screens/Search/styles.ts
+++ src/screens/Search/styles.ts
@@ -1,23 +1,85 @@
 import { scale } from "../../utils/scalling";
 
 import styled from "styled-components/native";
-
-
-
-
-export const SearchText = styled.Text`
-    font-size: ${scale(13)}px;
-    font-family: "Trueno-Regular";
-    color: ${(props) => props.theme.primarySuperDarkBlue};
-    text-align: center;
-`;
+import { Searchbar } from "react-native-paper";
+import DropDownPicker from "react-native-dropdown-picker";
 
 export const SearchTabContainer = styled.View`
-    width: 80%;
     align-items: center;
     justify-content: center;
-    
-    /* border-radius: ${scale(10)}px; */
-    background-color: #000000;
+    width: 85%;
 `;
+
+export const DropDownsContainer = styled.View`
+    align-items: center;
+    flex-direction: row;
+    justify-content: center;
+    width: 85%;
+`;
+
+export const UfDropDown = styled(DropDownPicker).attrs((props) => ({
+    placeholder: "UF",
+    containerStyle: {
+        height: scale(42),
+        width: '30%',
+    },
+    dropDownStyle: {
+        backgroundColor: props.theme.primaryWhite,
+        borderTopLeftRadius: scale(15),
+        borderTopRightRadius: scale(15),
+        borderBottomLeftRadius: scale(15),
+        borderBottomRightRadius: scale(15),
+        height: scale(80)
+    },
+    labelStyle: {
+        fontFamily: "Trueno-Regular",
+        fontSize: scale(16),
+        textAlign: "center",
+        color: props.theme.primaryDarkBlue,
+    },
+    activeLabelStyle: {
+        color: props.theme.primaryDarkBlue,
+        fontSize: scale(17),
+        fontFamily: "Trueno-SemiBold",
+    },
+    arrowColor: props.theme.primaryRed,
+    arrowSize: 18,
+}))`
+    border: 0;
+    margin-right: ${scale(5)}px;
+    background-color: ${(props) => props.theme.primaryWhite};
+`;
+
+export const CityDropDown = styled(DropDownPicker).attrs((props) => ({
+    placeholder: "Cidade",
+    containerStyle: {
+        height: scale(42),
+        width: '70%',
+    },
+    dropDownStyle: {
+        backgroundColor: props.theme.primaryWhite,
+        borderTopLeftRadius: scale(15),
+        borderTopRightRadius: scale(15),
+        borderBottomLeftRadius: scale(15),
+        borderBottomRightRadius: scale(15),
+        height: scale(200)
+    },
+    labelStyle: {
+        fontFamily: "Trueno-Regular",
+        fontSize: scale(16),
+        textAlign: "center",
+        color: props.theme.primaryDarkBlue,
+    },
+    activeLabelStyle: {
+        color: props.theme.primaryDarkBlue,
+        fontSize: scale(17),
+        fontFamily: "Trueno-SemiBold",
+    },
+    arrowColor: props.theme.primaryRed,
+    arrowSize: 18,
+}))`
+    border: 0;
+    background-color: ${(props) => props.theme.primaryWhite};
+`;
+
 
