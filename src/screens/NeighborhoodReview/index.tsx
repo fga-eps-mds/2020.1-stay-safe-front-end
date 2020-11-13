5b281a400c5b92af6f4ec9ed41c424a76beb5163
Herick Portugues <herick.portugues@gmail.com>
2020-11-13 17:43:23 -0300

    Refactor code with new NeighborhoodInfo component.

--- src/screens/NeighborhoodReview/index.tsx
+++ src/screens/NeighborhoodReview/index.tsx
@@ -1,21 +1,13 @@
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons, Feather } from "@expo/vector-icons";
 import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
-import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useTheme } from "styled-components";
 
 import HeaderTitle from "../../components/HeaderTitle";
-import {
-    InfoModal,
-    InfoTitle,
-    InfoContainer,
-    Info,
-    InfoColor,
-    InfoText,
-    InfoSubText,
-} from "../../components/InfoModal";
+import { InfoButton } from "../../components/InfoModal";
 import NeighborhoodImpressions from "../../components/NeighborhoodImpressions";
+import NeighborhoodInfo from "../../components/NeighborhoodInfo";
 import { KeyboardScrollView } from "../../components/NormalForms";
 import { Neighborhood } from "../../interfaces/neighborhood";
 import { scale } from "../../utils/scalling";
@@ -51,21 +43,9 @@ const NeighborhoodReview: React.FC = () => {
         if (neighborhood.statistics) setHasStatistics(true);
     }, []);
 
-    const getInfoText = (index: number) => {
-        if (index === 0) return "< 40%*";
-        if (index === 1) return "40% - 70%*";
-        else return "> 70%*";
-    };
-
-    const getInfoColor = (index: number) => {
-        if (index === 0) return theme.primaryImpressionGreen;
-        if (index === 1) return theme.primaryImpressionOrange;
-        else return theme.primaryImpressionRed;
-    };
-
     return (
         <SafeAreaView style={{ flex: 1 }}>
-            <HeaderTitle text="Avaliação média" goBack />
+            <HeaderTitle text={neighborhood.neighborhood} goBack />
             <KeyboardScrollView
                 style={{ backgroundColor: theme.primaryBackground }}
             >
@@ -75,6 +55,13 @@ const NeighborhoodReview: React.FC = () => {
                             <NeighborhoodText>
                                 Avaliação Geral dos Usuários
                             </NeighborhoodText>
+                            <InfoButton onPress={() => setIsInfoOpen(true)}>
+                                <Feather
+                                    name="info"
+                                    size={scale(25)}
+                                    color={theme.primaryGray}
+                                />
+                            </InfoButton>
                             <TitleCity>{`${neighborhood.neighborhood} - ${neighborhood.city}`}</TitleCity>
                             <StarContainer>
                                 <MaterialCommunityIcons
@@ -112,32 +99,10 @@ const NeighborhoodReview: React.FC = () => {
                     <EvaluateButtontText>Avaliar Bairro</EvaluateButtontText>
                 </EvaluateButton>
             </KeyboardScrollView>
-
-            <InfoModal
-                style={{ height: scale(240) }}
+            <NeighborhoodInfo
                 isOpen={isInfoOpen}
                 onClosed={() => setIsInfoOpen(false)}
-                swipeToClose={false}
-                position="center"
-                backButtonClose
-            >
-                <View style={{ alignItems: "center" }}>
-                    <InfoTitle>Legenda:</InfoTitle>
-                    <InfoContainer>
-                        {[...Array(3)].map((_, index) => {
-                            return (
-                                <Info key={index}>
-                                    <InfoColor color={getInfoColor(index)} />
-                                    <InfoText>{getInfoText(index)}</InfoText>
-                                </Info>
-                            );
-                        })}
-                    </InfoContainer>
-                    <InfoSubText style={{ marginBottom: 10 }}>
-                        * Porcentagem das avaliações positivas
-                    </InfoSubText>
-                </View>
-            </InfoModal>
+            />
         </SafeAreaView>
     );
 };
