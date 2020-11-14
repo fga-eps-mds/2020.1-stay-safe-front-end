4c4c21a0905d356cdbb851a796f18ce8da34d3c7
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-11-13 22:08:15 -0300

    Fix some warnings of eslint

--- src/services/neighborhood.tsx
+++ src/services/neighborhood.tsx
@@ -1,41 +1,41 @@
 import { userApi } from "./api";
 
 interface Neighborhood {
-    city: string,
-    id_neighborhood: number,
-    neighborhood: string,
-    state: string
+    city: string;
+    id_neighborhood: number;
+    neighborhood: string;
+    state: string;
 }
 
 export const getAllNeighborhoods = async () => {
-    const response = await fetch (userApi + `/neighborhood`, {
+    const response = await fetch(userApi + `/neighborhood`, {
         method: "GET",
         headers: {
-            Accept: "application/json"
+            Accept: "application/json",
         },
     });
 
     return { status: response.status, body: await response.json() };
-}
+};
 
 export const getNeighborhoodByState = async (state: string) => {
-    const response = await fetch (userApi + `/neighborhood?state=${state}`, {
+    const response = await fetch(userApi + `/neighborhood?state=${state}`, {
         method: "GET",
         headers: {
-            Accept: "application/json"
+            Accept: "application/json",
         },
     });
 
     return { status: response.status, body: await response.json() };
-}
+};
 
 export const getNeighborhoodByCity = async (city: string) => {
-    const response = await fetch (userApi + `/neighborhood?city=${city}`, {
+    const response = await fetch(userApi + `/neighborhood?city=${city}`, {
         method: "GET",
         headers: {
-            Accept: "application/json"
+            Accept: "application/json",
         },
     });
 
     return { status: response.status, body: await response.json() };
-}
+};
