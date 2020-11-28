eb3aaa32cdd986f632b66860d562e2339f1a5477
Herick Portugues <herick.portugues@gmail.com>
2020-11-28 12:30:55 -0300

    Show all cities when there is no uf selected and add some styles.

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -11,46 +11,61 @@ import { dropdownStyle } from "../../components/Dropdown";
 import HeaderTitle from "../../components/HeaderTitle";
 import { coordinatesDF } from "../../components/HeatMap/coordinates/coordinatesDF";
 import coordinatesSP from "../../components/HeatMap/coordinates/coordinatesSP.json";
-import {
-    KeyboardScrollView,
-    Container,
-    NormalInput,
-} from "../../components/NormalForms";
+import Loader from "../../components/Loader";
+import { Container, NormalInput } from "../../components/NormalForms";
 import { scale } from "../../utils/scalling";
-import { UfDropDown } from "./styles";
+import { SearchBarContainer, SearchIcon, UfDropDown } from "./styles";
+
+interface City {
+    name: string;
+    uf: string;
+}
 
 const Search: React.FC = () => {
     const theme = useTheme();
     const navigation = useNavigation();
 
-    const [DFCities, setDFCities] = useState<string[]>([]);
-    const [SPCities, setSPCities] = useState<string[]>([]);
+    const [DFCities, setDFCities] = useState<City[]>([]);
+    const [SPCities, setSPCities] = useState<City[]>([]);
 
-    const [selectedUf, setSelectedUf] = useState("df");
+    const [selectedUf, setSelectedUf] = useState("");
+
+    const [isLoading, setIsLoading] = useState(false);
 
     const [search, setSearch] = useState("");
-    const [filteredCities, setFilteredCities] = useState<String[]>([]);
-    const [citiesData, setCitiesData] = useState<String[]>([]);
+    const [filteredCities, setFilteredCities] = useState<City[]>([]);
+    const [citiesData, setCitiesData] = useState<City[]>([]);
 
     useEffect(() => {
+        setIsLoading(true);
+
         coordinatesDF.sort((a, b) =>
             ("" + a.name.normalize("NFD")).localeCompare(
                 b.name.normalize("NFD")
             )
         );
-        const cities = coordinatesDF.map((city) => {
-            return city.name;
+        const citiesDf = coordinatesDF.map((city) => {
+            return { name: city.name, uf: "df" };
         });
-        setDFCities(cities);
-        setFilteredCities(cities);
-        setCitiesData(cities);
-    }, []);
+        setDFCities(citiesDf);
 
-    useEffect(() => {
-        const cities = coordinatesSP[0].cities.map((city) => {
-            return city.name;
+        const citiesSp = coordinatesSP[0].cities.map((city) => {
+            return { name: city.name, uf: "sp" };
         });
-        setSPCities(cities);
+        setSPCities(citiesSp);
+
+        var allCities: City[] = citiesDf.concat(citiesSp);
+
+        allCities.sort((a, b) =>
+            ("" + a.name.normalize("NFD")).localeCompare(
+                b.name.normalize("NFD")
+            )
+        );
+
+        setFilteredCities(allCities);
+        setCitiesData(allCities);
+
+        setIsLoading(false);
     }, []);
 
     useEffect(() => {
@@ -66,87 +81,83 @@ const Search: React.FC = () => {
     const searchFilterFunction = (text: string) => {
         if (text) {
             const newData = citiesData.filter((city) => {
-                const itemData = city ? city.toUpperCase() : "".toUpperCase();
+                const itemData = city
+                    ? city.name.toUpperCase()
+                    : "".toUpperCase();
                 const textData = text.toUpperCase();
                 return itemData.indexOf(textData) > -1;
             });
             setFilteredCities(newData);
-            setSearch(text);
         } else {
             setFilteredCities(citiesData);
-            setSearch(text);
         }
+        setSearch(text);
     };
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <Container>
                 <HeaderTitle text="Pesquisar" />
-                <KeyboardScrollView>
-                    <View
-                        style={{
-                            width: "80%",
-                            flexDirection: "row",
-                            justifyContent: "space-between",
-                        }}
-                    >
-                        <UfDropDown
-                            items={[
-                                { label: "DF", value: "df", selected: true },
-                                { label: "SP", value: "sp" },
-                            ]}
-                            style={[
-                                dropdownStyle,
-                                { backgroundColor: theme.primaryWhite },
-                            ]}
-                            onChangeItem={(item) => {
-                                setSelectedUf(item.value);
-                            }}
-                        />
-                        <View style={{ width: "73%" }}>
-                            <NormalInput
-                                style={{ width: "100%" }}
-                                onChangeText={(text) => searchFilterFunction(text)}
-                                value={search}
-                                underlineColorAndroid="transparent"
-                                placeholder="Buscar cidade..."
-                            />
-                            <View
-                                style={{
-                                    position: "absolute",
-                                    right: "5%",
-                                    top: scale(12),
-                                }}
-                            >
-                                <Feather
-                                    name="search"
-                                    size={scale(18)}
-                                    color={theme.primaryBlack}
-                                />
-                            </View>
-                        </View>
-                    </View>
 
+                <SearchBarContainer>
+                    <UfDropDown
+                        items={[
+                            { label: "DF", value: "df" },
+                            { label: "SP", value: "sp" },
+                        ]}
+                        style={[
+                            dropdownStyle,
+                            { backgroundColor: theme.primaryWhite },
+                        ]}
+                        onChangeItem={(item) => {
+                            setSelectedUf(item.value);
+                            setSearch("");
+                        }}
+                    />
+
+                    <View style={{ width: "73%" }}>
+                        <NormalInput
+                            style={{ width: "100%" }}
+                            onChangeText={(text) => searchFilterFunction(text)}
+                            value={search}
+                            underlineColorAndroid="transparent"
+                            placeholder="Buscar cidade..."
+                        />
+                        <SearchIcon>
+                            <Feather
+                                name="search"
+                                size={scale(18)}
+                                color={theme.primaryBlack}
+                            />
+                        </SearchIcon>
+                    </View>
+                </SearchBarContainer>
+
+                {isLoading ? (
+                    <Loader />
+                ) : (
                     <FlatList
-                        style={{ width: "80%" }}
-                        contentContainerStyle={{ justifyContent: "center" }}
+                        style={{ marginBottom: scale(30) }}
+                        contentContainerStyle={{
+                            width: "80%",
+                            alignSelf: "center",
+                        }}
                         data={filteredCities}
                         keyExtractor={(item, index) => index.toString()}
                         renderItem={({ item, index }) => (
                             <TouchableCard
-                                key={index}
                                 onPress={() => {
                                     navigation.navigate("CityStatistics", {
-                                        city: item,
-                                        uf: selectedUf,
+                                        city: item.name,
+                                        uf: item.uf,
                                     });
                                 }}
                             >
-                                <CardContent>{item}</CardContent>
+                                <CardContent>{item.name}</CardContent>
                             </TouchableCard>
                         )}
                     />
-                </KeyboardScrollView>
+                )}
             </Container>
         </SafeAreaView>
     );
