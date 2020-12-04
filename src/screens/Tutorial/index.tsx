014f55969534e214eed5ceaa724dccb1150157cf
danieldagerom <danielprimo9@gmail.com>
2020-12-04 19:47:26 -0300

    add pagination logic
    Co-authored-by: Ítalo Alves <italooko@gmail.com>

--- src/screens/Tutorial/index.tsx
+++ src/screens/Tutorial/index.tsx
@@ -1,21 +1,30 @@
 import { useNavigation } from "@react-navigation/native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as Font from "expo-font";
 import React from "react";
+import { useUser } from "../../hooks/user";
+import { useTheme } from "styled-components";
+import { slides } from "./slidesScreens";
+import { scale } from "../../utils/scalling";
 
 import {
     TutorialSlider,
     Container,
+    ButtonContainer,
+    Button,
+    ButtonLabel,
     Title,
     ImageSlide,
-    Text
+    Description,
 } from "./styles";
-import { slides } from "./slidesScreens";
-import { useUser } from "../../hooks/user";
 
 const Tutorial: React.FC = () => {
     const navigation = useNavigation();
     const { updateShowTutorial } = useUser();
+    const theme = useTheme();
+
+    let slider;
 
     const [loaded] = Font.useFonts({
         "Trueno-SemiBold": require("../../fonts/TruenoSBd.otf"),
@@ -27,15 +36,30 @@ const Tutorial: React.FC = () => {
             <Container>
                 <Title>{item.title}</Title>
                 <ImageSlide source={item.image} />
-                <Text>{item.text}</Text>
+                <Description>{item.text}</Description>
             </Container>
         );
-    }
+    };
+
+    const _renderPagination = (activeIndex: number) => {
+        return (
+            <>
+                <ButtonContainer>
+                    <Button style={{ backgroundColor: theme.primaryLightBlue }}>
+                        <ButtonLabel>Voltar</ButtonLabel>
+                    </Button>
+                    <Button>
+                        <ButtonLabel>Avançar</ButtonLabel>
+                    </Button>
+                </ButtonContainer>
+            </>
+        );
+    };
 
     const endTutorial = async () => {
         await updateShowTutorial(false);
         navigation.navigate("HomeTabBar");
-    }
+    };
 
     if (!loaded) return null;
 
@@ -45,9 +69,11 @@ const Tutorial: React.FC = () => {
                 renderItem={renderSlide}
                 data={slides}
                 onDone={endTutorial}
+                renderPagination={_renderPagination}
+                ref={(ref) => (slider = ref)}
             />
         </SafeAreaView>
     );
-}
+};
 
 export default Tutorial;
