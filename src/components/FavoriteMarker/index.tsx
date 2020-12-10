d4adb7896ac0a5bd2be4ed5e6d82d29ee374681b
danieldagerom <danielprimo9@gmail.com>
2020-12-10 01:09:09 -0300

    add favorite place marker index
    Co-authored-by: Lucas Boaventura <lucasxboaventura@hotmail.com>

---
+++ src/components/FavoriteMarker/index.tsx
@@ -0,0 +1,50 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { Marker } from "react-native-maps";
+import { useTheme } from "styled-components";
+
+import DarkLogo from "../../img/logo-thief-dark.svg";
+import Logo from "../../img/logo-thief.svg";
+import { FavoritePlace } from "../../interfaces/favoriteplaces";
+import { Occurrence } from "../../interfaces/occurrence";
+import { scale } from "../../utils/scalling";
+
+interface FavoriteMarkerProps {
+    favoriteplace: FavoritePlace;
+}
+
+const FavoriteMarker: React.FC<FavoriteMarkerProps> = ({ favoriteplace }) => {
+    const theme = useTheme();
+    const navigation = useNavigation();
+
+
+    return (
+        <Marker
+            key={favoriteplace.id_place}
+            coordinate={{
+                latitude: favoriteplace.latitude,
+                longitude: favoriteplace.longitude,
+            }}
+            onPress={() =>
+                navigation.navigate("OccurrenceDetails", {
+                    occurrence,
+                })
+            }
+            tracksViewChanges={false}
+        >
+            {theme.type === "dark" ? (
+                <DarkLogo
+                    width={scale(38)}
+                    height={scale(38)}
+                />
+            ) : (
+                <Logo
+                    width={scale(38)}
+                    height={scale(38)}
+                />
+            )}
+        </Marker>
+    );
+};
+
+export default FavoriteMarker;
