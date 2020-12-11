4107c32caad65eb002181a0ac33e717e6a63bb64
Italo Alves <italooko@gmail.com>
2020-12-11 16:10:16 -0300

    About screen text and styles added

--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -1,7 +1,40 @@
+import * as Font from "expo-font";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import HeaderTitle from "../../components/HeaderTitle";
+import LogoContainer from "../../components/LogoContainer";
+import { Container } from "../../components/NormalForms";
+
+import { AboutText, AppTitle } from "./styles";
 
 const About: React.FC = () => {
-    return null;
+    const [loaded] = Font.useFonts({
+        "Age": require("../../fonts/Age.otf"),
+        "Trueno-Regular": require("../../fonts/TruenoRg.otf"),
+    });
+
+    if (!loaded) return null;
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <Container>
+                <HeaderTitle text="Sobre" goBack />
+                <LogoContainer />
+                <AppTitle>STAYSAFE</AppTitle>
+                <AboutText>
+                    A história do aplicativo StaySafe tem início em 2020,
+                    quando um grupo idealiza o projeto na disciplina de Métodos de Desenvolvimento de Software
+                    na Universidade de Brasília (UnB).
+                    {"\n\n"}
+                    O StaySafe é um aplicativo mobile para Android que mostra, através de mapas e estatísicas, 
+                    informações sobre a segurança de um lugar. Os dados exibidos são obtidos das Secretarias 
+                    de Segurança Pública e dos usuários do aplicativo que podem contribuir registrando uma 
+                    ocorrência da qual foram vítimas ou testemunhas, e avaliando a segurança dos bairros 
+                    que conhecem.
+                </AboutText>
+            </Container>
+        </SafeAreaView>
+    );
 };
 
 export default About;
