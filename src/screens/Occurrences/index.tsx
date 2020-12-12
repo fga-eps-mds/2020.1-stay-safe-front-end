a0813f4e4b28c2f21c8dbcde0a6221b940e8fb61
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-12-12 13:01:34 -0300

    Add the same layout of favorite places to corrences

--- src/screens/Occurrences/index.tsx
+++ src/screens/Occurrences/index.tsx
@@ -2,9 +2,12 @@ import { Feather } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import React, { useState, useEffect } from "react";
 import { TouchableOpacity } from "react-native";
+import Accordion from "react-native-collapsible/Accordion";
+import { RectButton } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useTheme } from "styled-components";
 
+import staySafeDarkMapStyle from "../..//styles/staySafeDarkMapStyle";
 import {
     ScrollViewStyled,
     CardContainer,
@@ -17,6 +20,8 @@ import {
 import HeaderTitle from "../../components/HeaderTitle";
 import Loader from "../../components/Loader";
 import StayAlert from "../../components/StayAlert";
+import StayMarker from "../../components/StayMarker";
+import StayNormalMap from "../../components/StayNormalMap";
 import { useUser } from "../../hooks/user";
 import { Occurrence } from "../../interfaces/occurrence";
 import {
@@ -33,6 +38,8 @@ const Occurrences: React.FC = () => {
     const [occurrences, setOccurrences] = useState<Occurrence[]>([]);
     const [showConfirmModal, setConfirmModal] = useState(false);
     const [idOccurrence, setIdOccurrence] = useState(0);
+
+    const [activeOccurrences, setActiveOccurrences] = useState([]);
 
     const [isLoading, setIsLoading] = useState(false);
 
@@ -77,11 +84,110 @@ const Occurrences: React.FC = () => {
         setConfirmModal(false);
     };
 
+    const _renderHeader = (
+        occurrence: Occurrence,
+        index: number,
+        isActive: boolean
+    ) => {
+        return (
+            <Card
+                style={{
+                    width: "100%",
+                    alignSelf: "center",
+                    marginBottom: 0,
+                    borderBottomLeftRadius: isActive ? 0 : scale(16),
+                    borderBottomRightRadius: isActive ? 0 : scale(16),
+                    borderBottomColor: isActive
+                        ? theme.primaryGray
+                        : theme.primaryBackground,
+                    borderBottomWidth: isActive ? 1 : 0,
+                }}
+            >
+                <CardData>
+                    <Title>{occurrence.occurrence_type}</Title>
+                    <Date>{occurrence.occurrence_date_time}</Date>
+                </CardData>
+
+                <CardActions>
+                    <TouchableOpacity
+                        onPress={() => {
+                            navigation.navigate("Occurrence", {
+                                occurrence,
+                            });
+                        }}
+                    >
+                        <Feather
+                            name="edit-3"
+                            size={scale(22)}
+                            color={theme.primarySuperDarkBlue}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setConfirmModal(true);
+                            setIdOccurrence(occurrence.id_occurrence);
+                        }}
+                    >
+                        <Feather
+                            name="trash-2"
+                            size={scale(22)}
+                            color={theme.primarySuperDarkBlue}
+                        />
+                    </TouchableOpacity>
+                </CardActions>
+            </Card>
+        );
+    };
+
+    const _renderContent = (
+        occurrence: Occurrence,
+        index: number,
+        isActive: boolean
+    ) => {
+        return (
+            <Card
+                style={{
+                    width: "100%",
+                    alignSelf: "center",
+                    marginBottom: 0,
+                    borderTopLeftRadius: isActive ? 0 : scale(16),
+                    borderTopRightRadius: isActive ? 0 : scale(16),
+                    borderBottomLeftRadius: scale(20),
+                    borderBottomRightRadius: scale(20),
+                }}
+            >
+                <StayNormalMap
+                    style={{ padding: scale(100) }}
+                    region={{
+                        latitude: occurrence.location[0],
+                        longitude: occurrence.location[1],
+                        latitudeDelta: 0.01,
+                        longitudeDelta: 0.01,
+                    }}
+                    zoomEnabled={false}
+                    scrollEnabled={false}
+                    loadingEnabled
+                    customMapStyle={
+                        theme.type === "dark" ? staySafeDarkMapStyle : []
+                    }
+                >
+                    <StayMarker occurrence={occurrence} />
+                </StayNormalMap>
+            </Card>
+        );
+    };
+
+    const updateSections = (activeSections) => {
+        setActiveOccurrences(
+            activeSections.includes(undefined) ? [] : activeSections
+        );
+    };
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <HeaderTitle text="Minhas Ocorrências" goBack />
             <ScrollViewStyled>
-                <CardContainer>
+                <CardContainer style={{ justifyContent: "center" }}>
                     {occurrences.length === 0 ? (
                         <Card style={isLoading && { justifyContent: "center" }}>
                             <CardData>
@@ -96,57 +202,28 @@ const Occurrences: React.FC = () => {
                             </CardData>
                         </Card>
                     ) : (
-                        occurrences.map((occurrence) => {
-                            return (
-                                <Card key={occurrence.id_occurrence}>
-                                    <CardData>
-                                        <Title>
-                                            {occurrence.occurrence_type}
-                                        </Title>
-                                        <Date>
-                                            {occurrence.occurrence_date_time}
-                                        </Date>
-                                    </CardData>
-
-                                    <CardActions>
-                                        <TouchableOpacity
-                                            onPress={() => {
-                                                navigation.navigate(
-                                                    "Occurrence",
-                                                    {
-                                                        occurrence,
-                                                    }
-                                                );
-                                            }}
-                                        >
-                                            <Feather
-                                                name="edit-3"
-                                                size={scale(22)}
-                                                color={
-                                                    theme.primarySuperDarkBlue
-                                                }
-                                            />
-                                        </TouchableOpacity>
-                                        <TouchableOpacity
-                                            onPress={() => {
-                                                setConfirmModal(true);
-                                                setIdOccurrence(
-                                                    occurrence.id_occurrence
-                                                );
-                                            }}
-                                        >
-                                            <Feather
-                                                name="trash-2"
-                                                size={scale(22)}
-                                                color={
-                                                    theme.primarySuperDarkBlue
-                                                }
-                                            />
-                                        </TouchableOpacity>
-                                    </CardActions>
-                                </Card>
-                            );
-                        })
+                        <Accordion
+                            containerStyle={{
+                                width: "80%",
+                                backgroundColor: theme.primaryBackground,
+                            }}
+                            sectionContainerStyle={{
+                                elevation: 5,
+                                marginBottom: scale(20),
+                                backgroundColor: theme.primaryBackground,
+                                borderTopLeftRadius: scale(20),
+                                borderTopRightRadius: scale(20),
+                                borderBottomLeftRadius: scale(20),
+                                borderBottomRightRadius: scale(20),
+                            }}
+                            sections={occurrences}
+                            activeSections={activeOccurrences}
+                            touchableComponent={RectButton}
+                            renderHeader={_renderHeader}
+                            renderContent={_renderContent}
+                            onChange={updateSections}
+                            expandMultiple
+                        />
                     )}
                 </CardContainer>
                 <StayAlert
