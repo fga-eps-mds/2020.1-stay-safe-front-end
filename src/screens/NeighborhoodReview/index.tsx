d164013314a6582ae7a8575ea654ab6ba462bb25
Herick Portugues <herick.portugues@gmail.com>
2020-12-13 14:11:15 -0300

    Correct some screens.

--- src/screens/NeighborhoodReview/index.tsx
+++ src/screens/NeighborhoodReview/index.tsx
@@ -12,6 +12,7 @@ import NeighborhoodInfo from "../../components/NeighborhoodInfo";
 import {
     KeyboardScrollView,
     ButtonWithIconLabel,
+    Container,
 } from "../../components/NormalForms";
 import { Neighborhood } from "../../interfaces/neighborhood";
 import { scale } from "../../utils/scalling";
@@ -47,67 +48,71 @@ const NeighborhoodReview: React.FC = () => {
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
-            <HeaderTitle text={neighborhood.neighborhood} goBack />
-            <KeyboardScrollView
-                style={{ backgroundColor: theme.primaryBackground }}
-            >
-                <StatisticsNeighborhoodCard style={{ elevation: 5 }}>
-                    {hasStatistics ? (
-                        <>
-                            <NeighborhoodText>
-                                Avaliação Geral dos Usuários
-                            </NeighborhoodText>
-                            <InfoButton onPress={() => setIsInfoOpen(true)}>
-                                <Feather
-                                    name="info"
-                                    size={scale(25)}
-                                    color={theme.primaryGray}
-                                />
-                            </InfoButton>
-                            <TitleCity>{`${neighborhood.neighborhood} - ${neighborhood.city}`}</TitleCity>
-                            <StarContainer>
-                                <MaterialCommunityIcons
-                                    name="star"
-                                    size={scale(50)}
-                                    color={theme.primaryStrongYellow}
-                                />
-                                <NeighborhoodAverage>
-                                    {neighborhood.statistics.average}
-                                </NeighborhoodAverage>
-                            </StarContainer>
-                            <ImpressionText>Impressões</ImpressionText>
-                            <NeighborhoodImpressions
-                                neighborhood={neighborhood}
-                            />
-                        </>
-                    ) : (
-                        <NeighborhoodText>
-                            Não há estatísticas para esse bairro.
-                        </NeighborhoodText>
-                    )}
-                </StatisticsNeighborhoodCard>
-                <Button
-                    width="70%"
-                    color={theme.primaryRed}
-                    style={{ marginTop: 30 }}
-                    onPress={() =>
-                        navigation.navigate("Rating", {
-                            rating: { neighborhood },
-                        })
-                    }
+            <Container>
+                <HeaderTitle text={neighborhood.neighborhood} goBack />
+                <KeyboardScrollView
+                    style={{ backgroundColor: theme.primaryBackground }}
                 >
-                    <MaterialCommunityIcons
-                        name="pencil"
-                        size={scale(18)}
-                        color={theme.primaryWhite}
-                    />
-                    <ButtonWithIconLabel>Avaliar Bairro</ButtonWithIconLabel>
-                </Button>
-            </KeyboardScrollView>
-            <NeighborhoodInfo
-                isOpen={isInfoOpen}
-                onClosed={() => setIsInfoOpen(false)}
-            />
+                    <StatisticsNeighborhoodCard style={{ elevation: 5 }}>
+                        {hasStatistics ? (
+                            <>
+                                <NeighborhoodText>
+                                    Avaliação Geral dos Usuários
+                                </NeighborhoodText>
+                                <InfoButton onPress={() => setIsInfoOpen(true)}>
+                                    <Feather
+                                        name="info"
+                                        size={scale(25)}
+                                        color={theme.primaryGray}
+                                    />
+                                </InfoButton>
+                                <TitleCity>{`${neighborhood.neighborhood} - ${neighborhood.city}`}</TitleCity>
+                                <StarContainer>
+                                    <MaterialCommunityIcons
+                                        name="star"
+                                        size={scale(50)}
+                                        color={theme.primaryStrongYellow}
+                                    />
+                                    <NeighborhoodAverage>
+                                        {neighborhood.statistics.average}
+                                    </NeighborhoodAverage>
+                                </StarContainer>
+                                <ImpressionText>Impressões</ImpressionText>
+                                <NeighborhoodImpressions
+                                    neighborhood={neighborhood}
+                                />
+                            </>
+                        ) : (
+                            <NeighborhoodText>
+                                Não há estatísticas para esse bairro.
+                            </NeighborhoodText>
+                        )}
+                    </StatisticsNeighborhoodCard>
+                    <Button
+                        width="70%"
+                        color={theme.primaryRed}
+                        style={{ marginTop: 30 }}
+                        onPress={() =>
+                            navigation.navigate("Rating", {
+                                rating: { neighborhood },
+                            })
+                        }
+                    >
+                        <MaterialCommunityIcons
+                            name="pencil"
+                            size={scale(18)}
+                            color={theme.primaryWhite}
+                        />
+                        <ButtonWithIconLabel>
+                            Avaliar Bairro
+                        </ButtonWithIconLabel>
+                    </Button>
+                </KeyboardScrollView>
+                <NeighborhoodInfo
+                    isOpen={isInfoOpen}
+                    onClosed={() => setIsInfoOpen(false)}
+                />
+            </Container>
         </SafeAreaView>
     );
 };
